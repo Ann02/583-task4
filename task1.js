9b12d3ba494d5db81f2ffbc900cf6ad81105f0cd
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-06 16:47:04 +0300

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,13 @@
+function calcRectangleArea(width, height){
+    let s = width * height;}
+    debugger;
+try {
+    console.log('It is good');
+} catch (error) {
+    if (width <= 0 && height <= 0 ) {
+        throw new Error("Number must be positive");
+    }
+    console.log('It is bad');
+}
+//calcRectangleArea(-2,3)
+//calcRectangleArea(2,3)
