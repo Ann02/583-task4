8a81972faba9cc2badf851c36348ab906deacc02
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-04-08 00:10:30 +0300

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,49 +1,49 @@
 let user1 = {
-    id: 7
+  id: 7,
 };
 
 let user2 = {
-    id: -12
+  id: -12,
 };
 
 let user3 = {
-    id: 44
+  id: 44,
 };
 
 let user4 = {
-    id: 22
+  id: 22,
 };
 
 let arr = [user1, user2, user3, user4];
 
 function showUser(id) {
+  if (id < 0) {
+    throw new Error("ID must not be negative: " + id);
+  }
 
-    if (id < 0) {
-        throw new Error("ID must not be negative: " + id);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id == id) {
+      return arr[i];
     }
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id == id) {
-            return arr[i];
-        }
-    }
+  }
 }
 
 function showUsers() {
-    try {
-        for (let i = 0; i < arguments.length; i++) {
-            showUser(arguments[i]);
-        }
-    } catch (error) {
-        console.log(error.message);
+  try {
+    for (let i = 0; i < arguments.length; i++) {
+      showUser(arguments[i]);
     }
-    let idArr = []
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id >= 0) {
-            idArr[i] = arr[i];
-        }
+  } catch (error) {
+    console.log(error.message);
+  }
+  let idArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id >= 0) {
+      idArr[i] = arr[i];
     }
-    console.log(idArr);
+  }
+  console.log(idArr);
 }
 
 //showUsers(7, -12, 44, 22);
+
